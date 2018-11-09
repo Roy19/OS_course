04a7371cdc890db6e910df5a07d0b69209f2a73d
Aritra Sur Roy <aritra1997@hotmail.com>
2018-11-09 16:08:48 +0530

    Initial commit

---
+++ Pthreads_tutorial/pthread-creation.c
@@ -0,0 +1,29 @@
+/* PThread Creation */ 
+
+#include <stdio.h>
+#include <pthread.h>
+
+void *foo (void *arg) {		/* thread main */
+	printf("Foobar!\n");
+	pthread_exit(NULL);
+}
+
+int main (void) {
+
+	int i;
+	pthread_t tid;
+	
+	pthread_attr_t attr;
+	pthread_attr_init(&attr); // Required!!!
+	
+	// create detachable thread
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+	
+	// create the thread
+	pthread_create(&tid, &attr, foo, NULL);
+
+	return 0;
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+These are some exercises for my OS course from
+Udacity.
